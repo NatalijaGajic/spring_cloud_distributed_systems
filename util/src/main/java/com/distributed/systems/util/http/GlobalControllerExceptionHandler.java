addf0565f116187c3d9c42036258e5ebb36717b3
NatalijaGajic <nat.gaj98@gmail.com>
2022-06-20 14:27:30 +0200

    Global error handling fix in util

--- util/src/main/java/com/distributed/systems/util/http/GlobalControllerExceptionHandler.java
+++ util/src/main/java/com/distributed/systems/util/http/GlobalControllerExceptionHandler.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.servlet.http.HttpServletRequest;
+
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
 
@@ -21,20 +23,21 @@ public class GlobalControllerExceptionHandler {
     @ResponseStatus(NOT_FOUND)
     @ExceptionHandler(NotFoundException.class)
     public @ResponseBody
-    HttpErrorInfo handleNotFoundExceptions(ServerHttpRequest request, Exception ex) {
+    HttpErrorInfo handleNotFoundExceptions(HttpServletRequest request, Exception ex) {
 
         return createHttpErrorInfo(NOT_FOUND, request, ex);
     }
 
     @ResponseStatus(UNPROCESSABLE_ENTITY)
     @ExceptionHandler(InvalidInputException.class)
-    public @ResponseBody HttpErrorInfo handleInvalidInputException(ServerHttpRequest request, Exception ex) {
+    public @ResponseBody HttpErrorInfo handleInvalidInputException(HttpServletRequest request, Exception ex) {
 
         return createHttpErrorInfo(UNPROCESSABLE_ENTITY, request, ex);
     }
 
-    private HttpErrorInfo createHttpErrorInfo(HttpStatus httpStatus, ServerHttpRequest request, Exception ex) {
-        final String path = request.getPath().pathWithinApplication().value();
+    private HttpErrorInfo createHttpErrorInfo(HttpStatus httpStatus, HttpServletRequest request, Exception ex) {
+        //TODO: path
+        final String path = request.getContextPath();
         final String message = ex.getMessage();
 
         LOG.debug("Returning HTTP status: {} for path: {}, message: {}", httpStatus, path, message);
