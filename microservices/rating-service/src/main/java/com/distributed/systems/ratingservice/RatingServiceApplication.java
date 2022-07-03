8eb36feb4241e0be83a20dffb6522d607e4396d4
NatalijaGajic <nat.gaj98@gmail.com>
2022-07-03 09:58:07 +0200

    MySQL connection setup

--- microservices/rating-service/src/main/java/com/distributed/systems/ratingservice/RatingServiceApplication.java
+++ microservices/rating-service/src/main/java/com/distributed/systems/ratingservice/RatingServiceApplication.java
@@ -1,15 +1,24 @@
 package com.distributed.systems.ratingservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.distributed.systems")
 public class RatingServiceApplication {
 
+	private static final Logger LOG = LoggerFactory.getLogger(RatingServiceApplication.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(RatingServiceApplication.class, args);
+
+		ConfigurableApplicationContext ctx = SpringApplication.run(RatingServiceApplication.class, args);
+
+		String mysqlUri = ctx.getEnvironment().getProperty("spring.datasource.url");
+		LOG.info("Connected to MySQL: " + mysqlUri);
 	}
 
 }
